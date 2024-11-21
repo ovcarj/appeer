329e9531c1df4364344844ebde4609457bf674df
ovcarj <juraj.ovcar@gmail.com>
2024-11-21 23:38:05 +0100

    Clean up and automatize command line definition

---
+++ src/appeer/cmdline/__init__.py
@@ -0,0 +1,19 @@
+import click
+import pkgutil
+import importlib
+
+@click.group(name='appeer')
+def appeer_cli():
+    pass
+
+for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
+
+    if module_name.startswith('cmd'):
+
+        _cmd_name = module_name.split('_')[1]
+        _cmd_cli = f'{_cmd_name}_cli'
+
+        _module = loader.find_module(module_name).load_module(module_name)
+        _command = getattr(_module, _cmd_cli)
+
+        appeer_cli.add_command(_command, name=_cmd_name)
