600b06443ebacf4dbfc1c11a1f3d50a040899de2
ovcarj <juraj.ovcar@gmail.com>
2025-02-20 02:47:40 +0100

    PARSE: Prepare parse actions through ParseJob
    ParseJob can now prepare parse actions, with each action corresponding
    to a file to be parsed.

--- src/appeer/parse/parse_job.py
+++ src/appeer/parse/parse_job.py
@@ -1,16 +1,44 @@
 """Parse scraped publications"""
 
 import os
+import threading
+import queue
 
 from appeer.general.datadir import Datadir
+from appeer.general import log as _log
+from appeer.general import utils as _utils
 
 from appeer.jobs.job import Job
-from appeer.parse import parse_reports as reports
+
+from appeer.parse.parse_action import ParseAction
+from appeer.parse.parse_packer import ParsePacker
 
 
 class ParseJob(Job, job_type='parse_job'): #pylint:disable=too-many-instance-attributes
     """
     Parse scraped publications
+
+    The ``mode`` attribute defines four available parsing modes:
+
+        1) Auto (A) [DEFAULT]
+            Parse files found through the ``scrape_jobs`` table,
+            for the scrape jobs that were executed but not previously parsed.
+            Note that only the scrape actions which were not previously parsed
+            will be parsed. Default mode
+
+        2) Everything (E) 
+            Parse all files found through the ``scrape_jobs`` table, regardless
+            of whether the jobs and the corresponding actions were previously
+            parsed. Only executed jobs are considered
+
+        3) Scrape job (S) 
+            Parse a list of scrape jobs with the given labels, regardless of 
+            whether the jobs and the corresponding actions were previously
+            parsed
+
+        4) File list (F) 
+            Parse a list of files. This mode is independent of previous
+            scrape jobs
 
     Instances of the ParseJob class have dynamically created properties
         corresponding to the columns of the ``parse_jobs`` table in the
@@ -35,7 +63,7 @@ class ParseJob(Job, job_type='parse_job'): #pylint:disable=too-many-instance-att
     parse_directory : str
         Directory into which to (temporarily) create files for parsing
     mode : str
-        Parsing mode; one of ('E', 'A', 'S', F); 'A' is default; mutable
+        Parsing mode; one of ('A', 'E', 'S', 'F'); 'A' is default
     job_status : str
         Job status; one of ('I', 'W', 'R', 'E', 'X'); mutable
     job_step : int
@@ -71,7 +99,7 @@ class ParseJob(Job, job_type='parse_job'): #pylint:disable=too-many-instance-att
 
     def _prepare_new_job_parameters(self, **kwargs):
         """
-        Helper method to prepare scrape parameters given to ``self.new_job()``
+        Helper method to prepare parse parameters given to ``self.new_job()``
 
         Keyword arguments are the same as the ones in ``self.new_job()``,
             excluding ``label``
@@ -85,7 +113,7 @@ class ParseJob(Job, job_type='parse_job'): #pylint:disable=too-many-instance-att
         parse_directory : str
             Directory into which to (temporarily) create files for parsing
         mode : str
-            Parsing mode; one of ('E', 'A', 'S', F); 'A' is default; mutable
+            Parsing mode; one of ('A', 'E', 'S', 'F'); 'A' is default
 
         Returns
         -------
@@ -119,3 +147,105 @@ class ParseJob(Job, job_type='parse_job'): #pylint:disable=too-many-instance-att
                 }
 
         return job_parameters
+
+    def add_publications(self, data_source=None):
+        """
+        Add publications to be parsed
+
+        Depending on the ``self.mode`` attribute, data_source may be:
+
+            1) (A): None
+            2) (E): None
+            3) (S): List of scrape job labels
+            4) (F): List of file paths
+
+        Parameters
+        ----------
+        data_source : None | list of str
+            None for modes ('A', 'E'), list of scrape job labels ('S'),
+                list of file paths ('F')
+
+        """
+
+        if not self._job_exists:
+            raise PermissionError('Cannot add publications to a parse job; most likely, the job has not yet been initialized.')
+
+        if self.job_status == 'X':
+            raise PermissionError(f'Cannot add new publications to the parse job "{self.label}"; the job has already been executed.')
+
+        if self.job_status == 'R':
+            raise PermissionError(f'Cannot add new publications to the parse job "{self.label}"; the job is in the "R" (Running) state.')
+
+        if self.job_status == 'E':
+            raise PermissionError(f'Cannot add new publications to the parse job "{self.label}"; the job is in the "E" (Error) state.')
+
+        self._job_mode = 'write'
+
+        self._queue = queue.Queue()
+        threading.Thread(target=self._log_server, daemon=True).start()
+
+        self.__prepare_parsing(data_source=data_source)
+
+        self._queue.join()
+
+    def __prepare_parsing(self, data_source):
+        """
+        Prepare parsing depending on the parsing mode
+
+        Parameters
+        ----------
+        data_source : None | list of str
+            None for modes ('A', 'E'), list of scrape job labels ('S'),
+                list of file paths ('F')
+
+        """
+
+        self._wlog(_log.boxed_message('PREPARING PARSING', centered=True) + '\n')
+
+        try:
+
+            packer = ParsePacker(parse_mode=self.mode,
+                    data_source=data_source,
+                    parse_directory=self.parse_directory,
+                    _queue=self._queue)
+
+        except ValueError as err:
+            self._wlog(err)
+
+        else:
+
+            packer.pack()
+
+            if packer.success:
+                self._queue.put(
+                        f'\n{len(packer.packet)} files are ready for parsing.')
+
+                self._add_actions(parse_packet=packer.packet)
+
+                self.no_of_publications = len(self.actions)
+                self.job_status = 'W'
+
+            else:
+                self._queue.put('No new actions were added to the parse job.')
+
+    def _add_actions(self, parse_packet):
+        """
+        Add ParseActions to the ParseJob
+
+        ``parse_packet`` is given by ``appeer.parse.parse_packer.packet``
+
+        Parameters
+        ----------
+        parse_packet : list of appeer.parse.parse_packer._ParseEntry
+            Packet containing info on the filepaths to be parsed
+                (and possibly scrape labels and action indices from which
+                the files originate)
+
+        """
+
+        for i, parse_entry in enumerate(parse_packet):
+
+            action = ParseAction(label=self.label,
+                    action_index=self.no_of_publications + i)
+
+            action.new_action(parse_entry=parse_entry)
