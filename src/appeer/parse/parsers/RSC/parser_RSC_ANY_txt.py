ea07d5f99281a2fee2994b1165f40eedb24fbb3a
ovcarj <juraj.ovcar@gmail.com>
2025-02-26 21:25:40 +0100

    PARSE: Implement DOI parsing for RSC_ANY_txt
    Two types of XML formats were encountered during testing parsing RSC
    journals. Therefore, implement DOI parsing for these two cases.

--- src/appeer/parse/parsers/RSC/parser_RSC_ANY_txt.py
+++ src/appeer/parse/parsers/RSC/parser_RSC_ANY_txt.py
@@ -1,5 +1,7 @@
 """Parser for any Royal Society of Chemistry (RSC) journal"""
 
+import functools
+
 from appeer.general import utils as _utils
 from appeer.parse.parsers.parser import Parser
 
@@ -57,3 +59,138 @@ class Parser_RSC_ANY_txt(Parser,
         """
 
         super().__init__(input_data=input_data)
+
+    @functools.cached_property
+    def doi(self):
+        """
+        Get the publication DOI
+
+        During testing RSC parsing, two formats of XML files were encountered;
+            therefore, the two cases are taken into account below
+
+        Returns
+        -------
+        _doi : str
+            The publication DOI
+
+        """
+
+        # RSC XML type (1)
+
+        _doi = _utils.get_doi_substring(self._input_data.title.text)
+
+        # RSC XML type (2)
+
+        if not _doi:
+
+            _meta = self._input_data.find('meta',
+                    attrs={'name': 'citation_doi'})
+
+            if _meta:
+
+                try:
+                    _doi_candidate = _meta.attrs['content']
+                    _doi = _utils.get_doi_substring(str(_doi_candidate))
+
+                except KeyError:
+                    pass
+
+        return _doi
+
+    @functools.cached_property
+    def publisher(self):
+        """
+        Get the publisher
+
+        Returns
+        -------
+        _publisher : str
+            The paper publisher
+        
+        """
+
+        return None
+
+    @functools.cached_property
+    def journal(self):
+        """
+        Get the journal
+
+        Returns
+        -------
+        _journal : str
+            The publication journal
+        
+        """
+
+        return None
+
+    @functools.cached_property
+    def title(self):
+        """
+        Get the publication title
+
+        Returns
+        -------
+        _title : str
+            Publication title
+        
+        """
+
+        return None
+
+    @functools.cached_property
+    def affiliations(self):
+        """
+        Get the author affiliations
+
+        Returns
+        -------
+        _affiliations : list of str
+            List of affiliations
+        
+        """
+
+        return None
+
+    @functools.cached_property
+    def received(self):
+        """
+        Get the date when the publication was received
+
+        Returns
+        -------
+        _received : str
+            The date when the publication was received
+        
+        """
+
+        return None
+
+    @functools.cached_property
+    def accepted(self):
+        """
+        Get the date when the publication was accepted
+
+        Returns
+        -------
+        _accepted : str
+            The date when the publication was accepted
+        
+        """
+
+        return None
+
+    @functools.cached_property
+    def published(self):
+        """
+        Get the date when the publication was published
+
+        Returns
+        -------
+        _published : str
+            The date when the publication was published
+        
+        """
+
+        return None
