3d78c29926d9ab793035c6e2a467b8d309f1e5f5
ovcarj <juraj.ovcar@gmail.com>
2025-03-09 01:01:45 +0100

    PARSE: Add create_and_run() to parse_scripts
    The create_and_run() function creates and runs a parse job.

--- src/appeer/parse/parse_scripts.py
+++ src/appeer/parse/parse_scripts.py
@@ -54,3 +54,56 @@ def append_publications(label, data_source):
     pj.add_publications(data_source=data_source)
 
     return pj
+
+def create_and_run(data_source,
+        label=None, cleanup=False,
+        **kwargs):
+    """
+    Create and run a parse job
+
+    For more details on the parameters, see documentation of ParseJob
+
+    Parameters
+    ----------
+    data_source : None | list of str
+        None for modes ('A', 'E'),
+            list of scrape job labels ('S'),
+            list of file paths ('F')
+    label : str
+        Unique job label
+    cleanup : bool
+        If True, the temporary parse directory will be deleted at the
+            end of the parse job
+
+    Keyword Arguments
+    -----------------
+    description : str
+        Optional job description
+    log_directory : str
+        Directory into which to store the log file
+    parse_directory : str
+        Directory into which to (temporarily) create files for parsing
+    mode : str
+        Parsing mode; one of ('A', 'E', 'S', F); 'A' is default
+    publishers : str | list of str | None
+        List of candidate parser publisher codes
+    journals : str | list of str | None
+        List of candidate parser journal codes
+    data_types : str | list of str | None
+        List of candidate parser data types;
+            currently, only 'txt' is supported
+    no_scrape_mark : bool
+        If True, scrape jobs will not be labeled as parsed
+            even if they are parsed successfully
+
+    """
+
+    pj = ParseJob(job_mode='write')
+
+    pj.new_job(label=label, **kwargs)
+
+    pj.add_publications(data_source=data_source)
+
+    pj.run_job(restart_mode='from_scratch',
+            cleanup=cleanup,
+            **kwargs)
