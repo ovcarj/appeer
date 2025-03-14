8b9e5784b8d88ebe039c382353b1b69b5007bdbc
ovcarj <juraj.ovcar@gmail.com>
2025-03-14 22:39:42 +0100

    COMMIT: Add commit_start_report() to commit reports
    Add report for the beginning of commit job execution.

--- src/appeer/commit/commit_reports.py
+++ src/appeer/commit/commit_reports.py
@@ -41,3 +41,54 @@ def commit_general_report(job, add_status_info=False):
     report += _log.boxed_message(msg) + '\n'
 
     return report
+
+def commit_start_report(job, run_parameters):
+    """
+    Return a formatted report at the beginning of commit job execution
+
+    Parameters
+    ----------
+    job : appeer.commit.commit_job.CommitJob
+        appeer commit job
+    run_parameters : dict
+        Dictionary containing restart_mode, no_parse_mark, overwrite
+
+    Returns
+    -------
+    report : str
+        Report on the beginning of commit job execution
+
+    """
+
+    current_time = _utils.get_current_datetime()
+    human_time = _utils.human_datetime(current_time)
+
+    report = '\n'
+
+    report += _log.boxed_message('COMMIT JOB EXECUTION', centered=True)
+
+    report += '\n'
+    report += _log.center(f'{human_time}')
+    report += '\n\n'
+
+    msg = ''
+    msg += f'{"no_of_publications":<20} {job.no_of_publications}\n'
+
+    for name, value in run_parameters.items():
+        msg += f'{name:<20} {value}\n'
+
+    msg = msg.rstrip('\n')
+
+    report += _log.boxed_message(msg, header='Job parameters')
+
+    report += '\n\n'
+
+    if run_parameters['restart_mode'] == 'from_scratch':
+        start_resume = 'Starting'
+
+    else:
+        start_resume = 'Resuming'
+
+    report += _log.boxed_message(f'{start_resume} commit job from step {job.job_step}/{job.no_of_publications - 1}')
+
+    return report
