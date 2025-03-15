645c8719392943202e38c0af1dcd800fac3211c1
ovcarj <juraj.ovcar@gmail.com>
2025-03-15 15:55:33 +0100

    COMMIT: Add create_and_run() to commit_scripts
    The create_and_run() function is used by the "appeer commit" command.

--- src/appeer/commit/commit_scripts.py
+++ src/appeer/commit/commit_scripts.py
@@ -80,3 +80,41 @@ def run_job(label,
     cj = CommitJob(label=label, job_mode='write')
     cj.run_job(restart_mode=restart_mode,
                **kwargs)
+
+def create_and_run(data_source,
+        label=None,
+        **kwargs):
+    """
+    Create and run a commit job
+
+    For more details on the parameters, see documentation of ParseJob
+
+    Parameters
+    ----------
+    data_source : None | list of str
+        None for modes ('A', 'E'),
+            list of parse job labels ('P'),
+    label : str
+        Unique job label
+
+    Keyword Arguments
+    -----------------
+    description : str
+        Optional job description
+    log_directory : str
+        Directory into which to store the log file
+    mode : str
+        Parsing mode; one of ('A', 'E', 'P'); 'A' is default
+    no_parse_mark : bool
+        If True, parse jobs will not be labeled as committed
+            even if they are committed successfully
+
+    """
+
+    cj = CommitJob(job_mode='write')
+
+    cj.new_job(label=label, **kwargs)
+
+    cj.add_publications(data_source=data_source)
+
+    cj.run_job(restart_mode='from_scratch', **kwargs)
